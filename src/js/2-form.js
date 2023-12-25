30996c414d35b03273d278ec99d5bd56070b86eb
Anastasia Romanyuk <vnstyv@gmail.com>
2023-12-25 20:48:04 +0200

    ed9

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -5,15 +5,15 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const storage = localStorage.getItem('feedback-form-state');
     if (storage) {
-        const { email, message } = JSON.parce(storage);
+        const { email, message } = JSON.parse(storage);
         feedbackForm.elements['email'].value = email;
         feedbackForm.elements['message'].value = message;
     }
     feedbackForm.addEventListener('input', function (event) {
         if (event.target.name === 'email' || event.target.name === 'message') {
             const currentValue = {
-                email: feedbackForm.elements['email'].value,
-                messsage: feedbackForm.elements['message'].value
+                email: feedbackForm.elements['email'].value.trim(),
+                message: feedbackForm.elements['message'].value.trim()
             };
             localStorage.setItem('feedback-form-state', JSON.stringify(currentValue));
         }
@@ -22,12 +22,12 @@ document.addEventListener('DOMContentLoaded', function () {
         event.preventDefault();
 
         const currentValue = {
-            email: feedbackForm.elements['email'].value,
-            message: feedbackForm.elements['message'].value
+            email: feedbackForm.elements['email'].value.trim(),
+            message: feedbackForm.elements['message'].value.trim()
         };
         console.log(currentValue);
 
         localStorage.removeItem('feedback-form-state');
-        feedbackForm.requestFullscreen();
+        feedbackForm.reset();
     });
 });
